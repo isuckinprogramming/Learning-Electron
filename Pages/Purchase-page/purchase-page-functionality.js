f6fe9529a85eaa383e6a9cc0fdf6f76abc58c866
Borgs <nisiyawa55@gmail.com>
2023-04-15 00:49:24 -0700

    Created the foundation for the purchase feature
    1.Added html elements for receiving inputs, for making transactions,
    the user needs to enter fields for identifying and modifying the items.
    for purchasing, the user needs to enter information, textfields
    and a button has been added to the purchase page.
    2.Added functions for the purchase functionality, the process for
    purchasing needs functions in order to keep code organize.
    I have created the names and bodies, but I haven't written the
    implementation of the code inside the function.
    The functions have names but no code inside so even if called nothing
    happens, I have not yet come up on how to actually do it yet. will
    make steady progress later.
    I added a comment to outline the process of the purchase.
    3.Added a css file for the html page, I still don't plan on actually
    adding any css yet, in preparation for future changes.

--- Pages/Purchase-page/purchase-page-functionality.js
+++ Pages/Purchase-page/purchase-page-functionality.js
@@ -9,23 +9,62 @@ displayItemsToPurchase();
 
 async function displayItemsToPurchase() { 
 
-    let itemsToPurchase = await window.receiveInventoryData.requestAndWaitForInventory();
-    
-    displayer.innerText = "";
+  let itemsToPurchase = await window.receiveInventoryData.requestAndWaitForInventory();
+  
+  displayer.innerText = "";
 
-    for (let i = 0; i < itemsToPurchase.length; i++ ) { 
-        
-        displayItem(itemsToPurchase[ i ])
-    }
+  for (let i = 0; i < itemsToPurchase.length; i++ ) { 
+      
+    displayItem(itemsToPurchase[ i ])
+  }
 
-    async function displayItem( item ) {
-        let itemData =
-            ` name: ${item.name} 
-              price: ${item.price}
-              quantity: ${item.quantity}
-            `
-        displayer.innerText += itemData;
-    }
-
+  async function displayItem( item ) {
+    let itemData =
+    ` name: ${item.name} 
+      price: ${item.price}
+      quantity: ${item.quantity}
+        `
+    displayer.innerText += itemData;
+  }
 }
 
+function purchase() {
+
+  function takeNameOfItemToPurchase() { 
+    
+    const nameTaker = document.getElementById("nameOfItemToPurchaseReceiver");
+    let nameOfItem = nameTaker.value;
+
+    
+
+    /*
+    *send name of item to inventory for handling, 
+    *and see if there is such an item that exist inside
+    *wait for response,
+    * if there is no item 
+    *   then return a value representing representing a failed outcome
+    *if outcome is successful 
+    *   then accept the query for the item and proceed
+    *to check if the amount the user wants to buy is valid
+    *   must not be 0 or lower and must not be a decimal number and must not exceed the available quantity
+    *if all conditions are clear 
+    *  then take the price of the item and multiply it with the amount to purchase
+    * take the amount of money the user has entered 
+    * examine the validity of the input of the user, 
+    *   must not be 0 or lower and the data type must be a number
+    * diminish the money the user entered with the calculated price
+    *   if the result is not a negative number then the user has succesfully purchased the item/items
+    *     diminish the amount of the item inside of the inventory by the amount the user purchased
+    *     tell user that the purchase is successful
+    *  
+    */
+  }
+
+  function takeQuantityOfItemToPurchase() {
+      
+  }
+
+  function takeMoneyForPuchase() { 
+   
+  }
+}
